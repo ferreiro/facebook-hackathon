05c03363de01f6e09cc777b6e87c7aa86fa829ed
Daniel Fernández Rico <deferrico@gmail.com>
2019-04-13 14:46:22 +0200

    Cross origin solved

--- packages/client/app/graph/Graph.js
+++ packages/client/app/graph/Graph.js
@@ -36,43 +36,43 @@ const Number = ({legend}) => {
 
 const demoData = [
   {
-    "name": "Page A",
+    "date": "Page A",
     "uv": 4000,
     "pv": 2400,
     "amt": 2400
   },
   {
-    "name": "Page B",
+    "date": "Page B",
     "uv": 3000,
     "pv": 1398,
     "amt": 2210
   },
   {
-    "name": "Page C",
+    "date": "Page C",
     "uv": 2000,
     "pv": 9800,
     "amt": 2290
   },
   {
-    "name": "Page D",
+    "date": "Page D",
     "uv": 2780,
     "pv": 3908,
     "amt": 2000
   },
   {
-    "name": "Page E",
+    "date": "Page E",
     "uv": 1890,
     "pv": 4800,
     "amt": 2181
   },
   {
-    "name": "Page F",
+    "date": "Page F",
     "uv": 2390,
     "pv": 3800,
     "amt": 2500
   },
   {
-    "name": "Page G",
+    "date": "Page G",
     "uv": 3490,
     "pv": 4300,
     "amt": 2100
@@ -85,7 +85,7 @@ const LineGraph = ({}) => {
       <LineChart width={730} height={250} data={demoData}
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="date" />
         <YAxis />
         <Tooltip />
         <Legend />
@@ -111,7 +111,7 @@ const AreaGraph = ({}) => {
             <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
           </linearGradient>
         </defs>
-        <XAxis dataKey="name" />
+        <XAxis dataKey="date" />
         <YAxis />
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
@@ -126,7 +126,15 @@ class Graph extends Component {
   componentDidMount() {
     const id = 2227226;
     const url = `localhost:5000/get_ad_stats?ad_id=${id}`;
-    fetch(url).then(r => console.log(r));
+    fetch(url).then(r => JSON.parse(r)).then(data => {
+      console.log(data);
+      data.map(d => ({
+        'date': new Date(d[0]),
+        'Success': d[1]/d[2],
+        'Spend': d[3],
+        'CTR': d[4]
+      }));
+    });
   }
 
   render() {
