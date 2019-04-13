f5fa6aff2148721d6eebfca2fd254f8690aba905
Daniel Fernández Rico <deferrico@gmail.com>
2019-04-13 14:36:07 +0200

    Area chart

--- packages/client/app/graph/Graph.js
+++ packages/client/app/graph/Graph.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
+import { AreaChart, Area, linearGradient, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
 import { Button, Tile } from 'carbon-components-react';
 
 const Number = ({legend}) => {
@@ -34,55 +34,55 @@ const Number = ({legend}) => {
   }
 }
 
-const LineGraph = ({}) => {
-  const data = [
-    {
-      "name": "Page A",
-      "uv": 4000,
-      "pv": 2400,
-      "amt": 2400
-    },
-    {
-      "name": "Page B",
-      "uv": 3000,
-      "pv": 1398,
-      "amt": 2210
-    },
-    {
-      "name": "Page C",
-      "uv": 2000,
-      "pv": 9800,
-      "amt": 2290
-    },
-    {
-      "name": "Page D",
-      "uv": 2780,
-      "pv": 3908,
-      "amt": 2000
-    },
-    {
-      "name": "Page E",
-      "uv": 1890,
-      "pv": 4800,
-      "amt": 2181
-    },
-    {
-      "name": "Page F",
-      "uv": 2390,
-      "pv": 3800,
-      "amt": 2500
-    },
-    {
-      "name": "Page G",
-      "uv": 3490,
-      "pv": 4300,
-      "amt": 2100
-    }
-  ];
+const demoData = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+];
 
+const LineGraph = ({}) => {
   return (
     <div className="c-graph">
-      <LineChart width={730} height={250} data={data}
+      <LineChart width={730} height={250} data={demoData}
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
@@ -96,9 +96,37 @@ const LineGraph = ({}) => {
   );
 }
 
+const AreaGraph = ({}) => {
+  return (
+    <div className="c-graph">
+      <AreaChart width={730} height={250} data={demoData}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+    </AreaChart>
+    </div>
+  );
+}
+
 class Graph extends Component {
   componentDidMount() {
-    // fetch('172.22.94.11:/get_tags_stats')
+    const id = 2227226;
+    const url = `localhost:5000/get_ad_stats?ad_id=${id}`;
+    fetch(url).then(r => console.log(r));
   }
 
   render() {
@@ -107,7 +135,7 @@ class Graph extends Component {
           <div className="bx--tile-container">
             <div className="bx--row">
               <div className="bx--col bx--col-md-12">
-                <LineGraph/>
+                <AreaGraph/>
               </div>
             </div>
             <div className="bx--row">
