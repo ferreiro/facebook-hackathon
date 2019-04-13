e98d16c428230957b7c0b27220e46b58b2c572b8
Daniel Fernández Rico <deferrico@gmail.com>
2019-04-13 15:30:15 +0200

    Correct data plotted

--- packages/client/app/graph/Graph.js
+++ packages/client/app/graph/Graph.js
@@ -80,21 +80,21 @@ const AreaGraph = ({data}) => {
       <AreaChart width={800} height={300} data={data}
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <defs>
-          <linearGradient id={variable} x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="var1" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
             <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
           </linearGradient>
-          <linearGradient id={variable} x1="0" y1="0" x2="0" y2="1">
+          {/* <linearGradient id="var2" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
             <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-          </linearGradient>
+          </linearGradient> */}
         </defs>
         <XAxis dataKey="date" />
         <YAxis />
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+        <Area type="monotone" dataKey={variable} stroke="#8884d8" fillOpacity={1} fill="url(#var1)" />
+        {/* <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#var2)" /> */}
     </AreaChart>
     </div>
   );
