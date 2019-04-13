7ae99c2344a05080b2abeb61edbee25b550464ad
Daniel Fernández Rico <deferrico@gmail.com>
2019-04-13 11:41:39 +0200

    Graph component and conflict solved

--- packages/client/app/Home.js
+++ packages/client/app/Home.js
@@ -1,12 +1,14 @@
 import React, {PureComponent} from 'react';
 
 import Header from './header/Header';
+import Graph from './Graph';
 
 class Home extends PureComponent {
     render() {
         return (
             <div className="campaign">
                 <Header />
+                <Graph />
             </div>
         )
     }
